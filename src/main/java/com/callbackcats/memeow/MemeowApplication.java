4b1d5fbffdc6e4b70b1c349de37e5df6e8a083e9
Anita Matei <matei.anita98@gmail.com>
2020-12-05 14:14:55 +0200

    Created project.

---
+++ src/main/java/com/callbackcats/memeow/MemeowApplication.java
@@ -0,0 +1,13 @@
+package com.callbackcats.memeow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/callbackcats/memeow/MemeowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callbackcats.memeow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
