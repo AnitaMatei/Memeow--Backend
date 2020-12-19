7eed3bbed025c4e2897a94b10e05b2390e27b11d
Anita Matei Gabriel <matei.anita98@gmail.com>
2020-12-19 20:30:18 +0200

    Added profile picture to user profiles.

--- src/main/java/com/callbackcats/memeow/service/AuthenticationService.java
+++ src/main/java/com/callbackcats/memeow/service/AuthenticationService.java
@@ -9,6 +9,7 @@ import com.callbackcats.memeow.model.entity.User;
 import com.callbackcats.memeow.repository.UserRepository;
 import com.callbackcats.memeow.security.JwtConstants;
 import com.callbackcats.memeow.security.JwtTokenGenerator;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
@@ -50,6 +51,7 @@ public class AuthenticationService {
         if (userOptional.isPresent()) {
             try {
                 jwtToken = facebookLoginUser(userOptional.get());
+                updateProfilePicture(userOptional.get(),facebookUser.getPictureUrl());
             } catch (ResponseStatusException ex) {
                 log.error(ex.getMessage());
                 return ResponseEntity.badRequest().body(ex.getMessage());
@@ -62,18 +64,21 @@ public class AuthenticationService {
 
     private FacebookUser verifyTokenValidity(FacebookAuthResponse facebookAuthResponse) {
         String fbUrl = JwtConstants.FACEBOOK_AUTH_URL + facebookAuthResponse.getAccessToken();
-        FacebookUser facebookUser = WebClient.create().get().uri(fbUrl).retrieve().
-                onStatus(HttpStatus::isError, clientResponse -> {
+        String jsonData = WebClient.create().get().uri(fbUrl).retrieve()
+
+                .onStatus(HttpStatus::isError, clientResponse -> {
                     throw new ResponseStatusException(clientResponse.statusCode(), "Invalid token");
                 })
-                .bodyToMono(FacebookUser.class).block();
-        log.info(facebookUser.toString());
-        return facebookUser;
+                .bodyToMono(String.class)
+                .block();
+
+
+        return new Gson().fromJson(jsonData, FacebookUser.class);
     }
 
     private String facebookLoginUser(User user) {
         if (user.getHasFacebook() == 0) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Existing email account");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Existing email account");
         }
 
         CustomUserPrincipal customUserPrincipal = new CustomUserPrincipal(modelMapper.map(user, UserDTO.class));
@@ -82,16 +87,22 @@ public class AuthenticationService {
     }
 
     private String facebookRegisterUser(FacebookUser facebookUser) {
-        User newUser = new User(facebookUser.getEmail(), facebookUser.getFirst_name(), facebookUser.getLast_name(), UUID.randomUUID().toString().replace("-",""));
+        User newUser = new User(facebookUser.getEmail(), facebookUser.getFirst_name(), facebookUser.getLast_name(), UUID.randomUUID().toString().replace("-", ""));
         newUser.setUserRole("ROLE_USER");
         newUser.setHasFacebook((byte) 1);
         newUser.setFacebookRegistrationDateUtc(new Timestamp(new Date().getTime()));
+        newUser.setIconUrl(facebookUser.getPictureUrl());
 
 
         userRepository.save(newUser);
         CustomUserPrincipal customUserPrincipal = new CustomUserPrincipal(modelMapper.map(newUser, UserDTO.class));
 
         return jwtTokenGenerator.generateTokenWithPrefix(customUserPrincipal);
+    }
+
+    private void updateProfilePicture(User user, String pictureUrl){
+        user.setIconUrl(pictureUrl);
+        userRepository.save(user);
     }
 
 }
